8ec3ec7eb7df85e4ac278bdd3bfc23e973414e94
Arifjawad18 <arif.jawad18@gmail.com>
2022-10-01 18:03:06 +0600

    nav style finisht

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useState,useEffect } from 'react';
-import logo from '../../images/favicon.ico.png'
-import Activity from '../Activity/Activity'
 import Blog from '../Blog/Blog';
 import Cart from '../Cart/Cart';
 import './Home.css'
+import logo from '../../images/favicon.ico.png'
+import Activity from '../Activity/Activity'
+
 const Home = () => {
     const [activities,setActivities] = useState([]);
     const [cart,setCart] = useState([]);
@@ -14,29 +15,33 @@ const Home = () => {
         .then(data=>setActivities(data))
     },[]);
     const handleCart = (activity) =>{
-        
        const newCart = [...cart,activity]
        setCart(newCart)
     }
     return (
         <div>
+            
             <div className="home-container">
                 <div className="left-side">
+               
+               <div className="nav">
+                <img src={logo} alt="" className='logo-img' />
+                </div>
+               
                     <div className="heading">
-                        <img src={logo} alt="" className='logo-img' />
-                        <span className='heading-text'> IELTS Daily</span>
+                        
+                        <span className='heading-text'> IELTS Daily Routine</span>
                     </div>
                     <h1 className='learning-text'>Now Select Your IELTS Learning</h1>
                     <div className="activity-container">
                     {
-                    activities.map((activity => <Activity 
+                    activities.map((activity => 
+                    <Activity 
                         activity={activity} 
                         key={activity?.id} 
                         handleCart ={handleCart}
-                       >
-                        </Activity>))
-                }
-                    </div>
+                    ></Activity>))}
+                </div>
                 </div>
                 <div className="right-side">
                     <Cart cart={cart}></Cart>
